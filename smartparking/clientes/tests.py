465273e485d15f4a4fd3ef786b29eaff684575eb
Ferreira.will <ferreirawill94@gmail.com>
2019-02-07 00:09:48 -0200

    Aula-153: Models, relação entre tabelas com foreign Key

---
+++ smartparking/clientes/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def clientes(request):
+    return HttpResponse("João e Maria e Shailaia")
+
+def cliente_detalhe(request,id):
+    return HttpResponse(id)
+
+
+def cliente_nome(request,nome):
+    return HttpResponse('Olá %s' %nome)
