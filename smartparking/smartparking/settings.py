f3e50efdff2ddd2157727e71404c73a093eddbfb
William Ferreira <ferreirawill94@gmail.com>
2019-02-07 22:08:27 -0200

    Aula 156- Alterações no django admin

--- smartparking/smartparking/settings.py
+++ smartparking/smartparking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*_w5vg+8ine3&&n87*cc%f@x^6%iz8(g-77ooq%6uj-z0_8=j%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','192.168.31.98']
 
 
 # Application definition
